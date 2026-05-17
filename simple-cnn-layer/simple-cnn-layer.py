2ccc27890a06d612baba85d081454844d46a86b4
Muhammad Hassan <connect.mhassan@gmail.com>
2026-05-17 10:52:39 +0500

    Add solution for simple-cnn-layer

---
+++ simple-cnn-layer/simple-cnn-layer.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def conv2d(x, W, b):
+
+    N, C_in, H, W_in = x.shape
+    C_out, _, KH, KW = W.shape
+    H_out = H - KH + 1
+    W_out = W_in - KW + 1
+
+    windows = np.lib.stride_tricks.sliding_window_view(x, (KH, KW), axis=(2, 3))
+    y = np.einsum('nchwkl, ockl -> nohw', windows, W, optimize=True)
+    y += b.reshape(1, -1, 1, 1)
+
+    return y
